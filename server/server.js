1a6394965c2880ca351391933e21dfdeaaf21ace
Harry <harold.william.bell@gmail.com>
2018-08-09 10:19:47 -0600

    Add GET todos/:id

--- server/server.js
+++ server/server.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 
+const ObjectId = require('mongodb').ObjectId;
+
 var mongoose = require('./db/mongoose.js').mongoose;
 var Todo = require('./models/todo.js').Todo;
 var User = require('./models/user.js').User;
@@ -29,6 +31,32 @@ app.get('/todos', (req, res) => {
     res.status(400).send(e);
   })
 })
+
+// GET /todos/1234324
+app.get('/todos/:id', (req, res) => {
+  var id = req.params.id;
+  
+  // validate id
+  if (!ObjectId.isValid(id)) {
+    return res.status(404).send("Invalid object ID");
+  }
+
+  // findById
+    // success
+      // if todo - send it back
+      // if no todo - send back 404 with emtpy body
+    // error
+      // 400 - and send empty body back
+  
+    Todo.findById(id).then((todo) => {
+      if (todo) {
+        res.send({"todo": todo});
+      } else {
+        res.status(404).send();
+      }
+    }).catch((e) => res.status(400).send());
+
+});
 
 app.listen(3000, () => {
   console.log('Started on port 3000');
--- server/tests/dummy.js
+++
@@ -1,7 +0,0 @@
-var o = {p: 42, q: true};
-// var {p, q} = o;
-
-var p = o.p;
-var q = o.q;
-
-console.log(p, q)
