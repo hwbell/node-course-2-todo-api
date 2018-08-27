982bf6c3e8990d474a4942d4221d4db3640f5177
Harry <harold.william.bell@gmail.com>
2018-08-27 07:40:48 -0600

    Add tests for PATCH /todos/:id

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -10,7 +10,9 @@ const todos = [{
   text: 'Fist test todo'
 }, {
   _id: new ObjectID(),
-  text: 'Second test todo'
+  text: 'Second test todo',
+  completed: true,
+  completedAt: 333
 }]
 
 beforeEach((done) => {
@@ -143,4 +145,63 @@ describe('DELETE /todos/:id', () => {
       .expect(404)
       .end(done);
   });
+
 }); 
+
+describe('PATCH /todos/:id', () => {
+  it('should update the todo', (done) => {
+    // grab id of first item
+    var hexId = todos[0]._id.toHexString();
+    var text = 'This should be the new text';
+
+    // update text, set completed = true
+    request(app)
+      .patch(`/todos/${hexId}`)
+      .send({
+        completed: true,
+        text: text
+      })
+    // 200
+      .expect(200)
+
+    // text is changed, completed is true, completedAt is a number
+      .expect((res) => {
+        expect(res.body.todo.text).toBe(text);
+        expect(res.body.todo.completed).toBe(true);
+        expect(res.body.todo.completedAt).toBeA('number');
+      })
+      .end(done);
+
+  })
+
+  it('should clear completedAt when todo is not completed', (done) => {
+    
+    // updte text, set completed false, 
+    // 200
+    // text is changed, completed false, completedAt is null
+
+    // grab id of first item
+    var hexId = todos[1]._id.toHexString();
+    var text = 'This should be the new text!!';
+
+    // update text, set completed = true
+    request(app)
+      .patch(`/todos/${hexId}`)
+      .send({
+        completed: false,
+        text: text
+      })
+    // 200
+      .expect(200)
+
+    // text is changed, completed is true, completedAt is a number
+      .expect((res) => {
+        expect(res.body.todo.text).toBe(text);
+        expect(res.body.todo.completed).toBe(false);
+        expect(res.body.todo.completedAt).toNotExist();
+      })
+      .end(done);
+
+  })
+
+})
